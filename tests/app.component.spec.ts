9aba4a1397ab83fd36e86d005f9f2e06c8dbba3f
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-04-02 17:40:58 -0500

    staring with tests

---
+++ tests/app.component.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../src/app/app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  test('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  test(`should have as title 'angular-testing'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    expect(component.title).toEqual('angular-testing');
+  });
+
+  test('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const component = fixture.componentInstance;
+    const h1 = compiled.querySelector('h1')
+
+    expect(h1?.textContent).toContain(component.title);
+  });
+
+  test('Debe hacer match con el snapshot', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+    fixture.detectChanges();
+    expect(compiled).toMatchSnapshot();
+
+  })
+});
